bf068a39073afe9f6a6b0a9ce4153d8f4b9ab436
jessicapei <jessicapei93@gmail.com>
2015-09-04 16:47:46 -0500

    correct +

--- js/flickr.js
+++ js/flickr.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 			//go through each photo in feed and return as <li>
 			$.each(data.items, function(i, photo) {
 				photoHTML += '<li class="grid-25 tablet-grid-50">';
-				photoHTML += '<a href="' + photo.link '" class="image">';
+				photoHTML += '<a href="' + photo.link + '" class="image">';
 				photoHTML += '<img src="' + photo.media.m + '"></a></li>';
 			});
 			photoHTML += "</ul>";
