28f2c5188afd4c1d13957fc64a881d63ffe44d59
jessicapei <jessicapei93@gmail.com>
2015-09-08 18:36:00 -0500

    removed carousel arrow gradients

--- js/flickr.js
+++ js/flickr.js
@@ -7,11 +7,15 @@ $(".gallerylink").click(function(event) {
 	function displayPhotos(data) {
 		var photoHTML = '';
 		$.each(data.photoset.photo, function(i, item) {
-			photoHTML += '<div class="col-xs-6 col-md-3 grid-item"><a href="#" class="thumbnail">';
+			photoHTML += '<div class="col-xs-6 col-md-3 grid-item">';
+			var img_link = "http://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_z.jpg";
+			photoHTML += '<a href="' + img_link + '" class="thumbnail">';
 			var img_src = "http://farm" + item.farm + ".staticflickr.com/" + item.server + "/" + item.id + "_" + item.secret + "_n.jpg";
-			photoHTML += '<img class="img-responsive" src="' + img_src + '"></a></div>';
+			photoHTML += '<img class="img-responsive" src="' + img_src + '" alt="' + item.title + '"></a></div>';
 		});
 		$('#img_gallery').html(photoHTML);
+
+		// MASONRY
 
 		$("#img_gallery").imagesLoaded(function() {
 			$('#img_gallery').masonry({
@@ -19,6 +23,54 @@ $(".gallerylink").click(function(event) {
 			  itemSelector: '.grid-item'
 			});
 		}).masonry('reloadItems');
+
+		// LIGHTBOX
+
+		var $image = $("<img>");
+		var $overlay = $('<div id="overlay"></div>');
+		var $caption = $("<p class='caption'></p>");
+		var $previous = $("<span><a href='#'>PREVIOUS</a></span>");
+		var $next = $("<span><a href='#'>NEXT</a></span>");
+
+		$overlay.append($previous, $next);
+
+		//add image to overlay
+		$overlay.append($image);
+
+		//add caption to overlay
+		$overlay.append($caption);
+
+		//add overlay to body
+		$("body").append($overlay);
+			//show an image
+			//show a caption
+
+		//capture the click event on a link to an image
+		$("#img_gallery a").click(function(event) {
+			event.preventDefault();
+			var image_href = $(this).attr("href");
+			//update overlay with the image linked in the link
+			$image.attr("src", image_href);
+			//show the overlay
+			$overlay.show();
+			//get child's alt attribute and set caption
+			var captionText = $(this).children("img").attr("alt");
+			//add text to the caption
+			$caption.text(captionText);
+		});
+
+		$next.click(function() {
+			event.preventDefault();
+			console.log("clicked");
+
+		});
+
+		//when overlay is clicked
+		$overlay.click(function() {
+			//hide the overlay
+			$overlay.hide();
+		});
+			
 	};
 
 	if ($(this).text() === "Landscapes") {
---
+++ js/lightbox.js
@@ -0,0 +1,17 @@
+//capture the click event on a link to an image
+$("#img_gallery a").click(function(event) {
+	event.preventDefault();
+	var href = $(this).attr("href");
+	console.log(href);
+	//show the overlay
+	//update overlay with the image linked in the link
+	//get child's alt attribute and set caption
+});
+
+
+//add overlay
+	//show an image
+	//show a caption
+
+//when overlay is clicked
+	//hide the overlay
